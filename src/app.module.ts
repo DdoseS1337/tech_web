98be24a654b6dbe8eb285e40861d5c8d19fc64d8
Yaroslav Maksymchuk <yaroslav.maksymchuk@gmail.com>
2023-11-27 11:59:36 +0200

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AuthModule } from './auth/auth.module';
 import { ImageModule } from './image/image.module';
 import { UsersModule } from './users/users.module';
 import { WebsocketModule } from './websocket/websocket.module';
+import { LoggerModule } from './logger/logger.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, ImageModule, UsersModule, WebsocketModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    ImageModule,
+    UsersModule,
+    WebsocketModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.number().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.useLogger(app.get(Logger));
+  app.use(cookieParser());
+  await app.listen(configService.get('HTTP_PORT'));
 }
 bootstrap();
